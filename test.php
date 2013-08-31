d519a085e7b5944b84a7c376bdb12f32d146d2f1
ec2-user@ip-10-212-97-41 <ec2-user@happinesslabs.com>
2013-08-31 10:52:39 +0000

    commited all changed files from html folder to git repo

--- test.php
+++ test.php
@@ -11,6 +11,6 @@ include('include/app-common-config.php');
 
 $post = $facebook->api('/100002349398425/notifications/', 'post',  array(
   'access_token' => APPID.'|'.SECRET,
-  'href' => 'http://www.google.com',  //this does link to the app's root, don't think this actually works, seems to link to the app's canvas page
+  'href' => 'https://www.happinesslabs.com/newuser_process.php',  
   'template' => 'Max 180 characters'));
 ?>
