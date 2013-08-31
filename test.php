671510528ad6bb5b1eb158c3922738fdfb40dc30
ec2-user@ip-10-212-97-41 <ec2-user@happinesslabs.com>
2013-08-31 10:07:38 +0000

    commited all changed files from html folder to git repo

--- test.php
+++ test.php
@@ -4,10 +4,37 @@ include_once "fbmain.php";
 include('include/app-common-config.php');
 $message='';
 
-  $user_id = USER_ID;
+  $user_id = FB_ID;
 
   $apprequest_url ="https://graph.facebook.com/".$user_id."/apprequests?message='NewInvite'"."&data='INSERT_STRING_DATA'&".$_SESSION['fb_access_token']."&method=post";
 
   $result = file_get_contents($apprequest_url);
   echo("App Request sent?", $result);
+  
+  
+?>
+
+<?php 
+/*
+  $app_id = APPID;
+  $app_secret = SECRET;
+
+  $token_url = "https://graph.facebook.com/oauth/access_token?" .
+    "client_id=" . $app_id .
+    "&client_secret=" . $app_secret .
+    "&grant_type=client_credentials";
+
+  $app_access_token = file_get_contents($token_url);
+
+  $user_id = '';
+
+  $apprequest_url ="https://graph.facebook.com/" .
+    $user_id .
+    "/apprequests?message=’INSERT_UT8_STRING_MSG’" . 
+    "&data=’INSERT_STRING_DATA’&"  .   
+    $app_access_token . “&method=post”;
+
+  $result = file_get_contents($apprequest_url);
+  echo(“Request id number: ”, $result);
+  */
 ?>
