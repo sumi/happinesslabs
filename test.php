ef0159e14e9c66c0d7a15d698a32850f4a8419d6
ec2-user@ip-10-212-97-41 <ec2-user@happinesslabs.com>
2013-08-31 10:43:38 +0000

    commited all changed files from html folder to git repo

--- test.php
+++ test.php
@@ -10,7 +10,7 @@ include('include/app-common-config.php');
 <?php 
 
 $post = $facebook->api('/100002349398425/notifications/', 'post',  array(
-  'access_token' => $_SESSION['fb_access_token'],
+  'access_token' => APPID.'|'.SECRET,
   'href' => 'google.com',  //this does link to the app's root, don't think this actually works, seems to link to the app's canvas page
   'template' => 'Max 180 characters'));
 ?>
