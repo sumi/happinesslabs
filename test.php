3b3bcb932939ecc7b6d650c2fb3ba824527c3c31
ec2-user@ip-10-212-97-41 <ec2-user@happinesslabs.com>
2013-08-31 10:16:37 +0000

    commited all changed files from html folder to git repo

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ include_once "fbmain.php";
 include('include/app-common-config.php');
 $message='';
 
-  $user_id = FB_ID;
+ echo"==>".$user_id = FB_ID;
 
   $apprequest_url ="https://graph.facebook.com/".$user_id."/apprequests?message='NewInvite'"."&data='INSERT_STRING_DATA'&".$_SESSION['fb_access_token']."&method=post";
 
