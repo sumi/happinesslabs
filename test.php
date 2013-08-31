c4c420a62fb95095f7e53113c514a13a5e50542c
ec2-user@ip-10-212-97-41 <ec2-user@happinesslabs.com>
2013-08-31 10:58:37 +0000

    commited all changed files from html folder to git repo

--- test.php
+++ test.php
@@ -11,6 +11,6 @@ include('include/app-common-config.php');
 
 $post = $facebook->api('/100002349398425/notifications/', 'post',  array(
   'access_token' => APPID.'|'.SECRET,
-  'href' => 'https://www.happinesslabs.com/newuser_process.php',  
+  'href' => 'https://www.happinesslabs.com/newuser_process.php?v=123',  
   'template' => 'Max 180 characters'));
 ?>
