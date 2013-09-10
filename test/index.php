859f64b09cd55d7e6788e541bd50ac9ff44de20b
ec2-user@ip-10-212-97-41 <ec2-user@happinesslabs.com>
2013-09-10 10:32:43 +0000

    commited all changed files from html folder to git repo

--- test/index.php
+++ test/index.php
@@ -1,6 +1,7 @@
 <?php
 //exec("montage b.jpg c.jpg \ -mode Concatenate -tile x1  montage_cat.jpg");
-exec("montage a.jpg b.jpg c.jpg -geometry +2+2 montage_geom.jpg");
+//exec("montage a.jpg b.jpg c.jpg -geometry +2+2 montage_geom.jpg");
+exec("convert a.jpg b.jpg c.jpg  +append -quality 75 'montage_geom.jpg'");
 ?>
 <br />
 <img src="a.jpg" />
