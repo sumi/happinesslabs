1d91c2707e44ee2079ebf45738c3200f318f3f3e
ec2-user@ip-10-212-97-41 <ec2-user@happinesslabs.com>
2013-09-10 10:09:39 +0000

    commited all changed files from html folder to git repo

--- test/index.php
+++ test/index.php
@@ -2,10 +2,10 @@
 exec("montage a.jpg b.jpg c.jpg \ -mode Concatenate -tile x1  montage_cat.jpg");
 ?>
 <br />
-<img src="a.jpg" height="300" width="300" />
+<img src="a.jpg" />
 <br />
-<img src="b.jpg" height="300" width="300" />
+<img src="b.jpg"/>
 <br />
-<img src="c.jpg" height="300" width="300" />
+<img src="c.jpg"/>
 ===============<br />================
-<img src="montage_cat.jpg" height="300" width="300" />
+<img src="montage_cat.jpg"/>
